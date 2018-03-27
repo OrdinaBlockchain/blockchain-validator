f5ea7652d7349b3d8f1e46f03d6f152b8bcb212d
Vidovitsch <david.de.bekker@gmail.com>
2018-03-27 11:10:20 +0200

    smoaksignal dependency added to validator.js

--- validator.js/validator.js
+++ validator.js/validator.js
@@ -1,2 +1,17 @@
 'use strict';
 require('dotenv').config();
+
+const smoke = require('smokesignal');
+
+const node = smoke.createNode({
+    port: process.env.PORT,
+    address: smoke.localIp(process.env.HOST),
+});
+
+node.on('connect', function() {
+    // Hey, now we have at least one peer!
+    // ...and broadcast stuff -- this is an ordinary duplex stream!
+    node.broadcast.write('HEYO! I\'m here');
+});
+
+node.start();
