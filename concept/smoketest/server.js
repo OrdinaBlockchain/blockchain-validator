610edb90b2ed9107c56bf8792355ecf7df90067f
Michiel van Eijkeren <m.vaneijkeren@gmail.com>
2018-03-27 13:56:22 +0200

    dynamic node connection

--- concept/smoketest/server.js
+++ concept/smoketest/server.js
@@ -1,49 +1,42 @@
-var smoke = require('smokesignal')
+let smoke = require('smokesignal')
 'use strict';
 
 var ip = "127.0.0.1";
 var port = 8495;
 var connected = false;
-var node;
+let node;
 
 function createNode(port) {
     node = smoke.createNode({
         port: port,
-        address: smoke.localIp(ip) // Tell it your subnet and it'll figure out the right IP for you
-            ,
-        seeds: [{ port: 8496, address: ip }] // the address of a seed (a known node)
+        address: smoke.localIp(ip),
+        seeds: [{ port: 8496, address: ip }]
     })
     if (node) { connected = true; }
 }
 
 function setNodeBehaviour() {
     node.on('connect', function() {
-        // Hey, now we have at least one peer!
-
-        // ...and broadcast stuff -- this is an ordinary duplex stream!
-        node.broadcast.write("i am here brotha's")
+        let message = { type: "new", "ip": ip, "port": port };
+        node.broadcast.write(JSON.stringify(message));
     })
+    node.on('disconnect', function() {})
 
-    node.on('disconnect', function() {
-            // Bah, all peers gone.
-        })
-        // Broadcast is a stream
-    process.stdin.pipe(node.broadcast).pipe(process.stdout)
+    process.stdin.pipe(node.broadcast).on('data', function(chunk) {
+        console.log(chunk.toString('utf8'));
+    });
 }
 
 function startNode(curport) {
-    try {
-        createNode(curport);
-        setNodeBehaviour()
-        node.start()
-    } catch (err) {
+    port = curport;
+    createNode(curport);
+    setNodeBehaviour();
+    node.on("error", function() {
         startNode(curport + 1);
-    }
+    });
+    node.start();
 }
 
-//Start the darn thing
 startNode(port);
-
-//node.broadcast.write("i am here brotha's")
 // mah, i'd rather stop it
 //node.stop()
--- concept/smoketest/server1.js
+++ concept/smoketest/server1.js
@@ -1,8 +1,8 @@
-var smoke = require('smokesignal')
+let smoke = require('smokesignal')
 
 'use strict';
 
-var node = smoke.createNode({
+let node = smoke.createNode({
     port: 8496,
     address: smoke.localIp("127.0.0.1") // Tell it your subnet and it'll figure out the right IP for you
 })
@@ -10,9 +10,7 @@ var node = smoke.createNode({
 // listen on network events...
 
 node.on('connect', function() {
-    // Hey, now we have at least one peer!
-    var newpeer = node.peers.list[node.peers.list.length - 1];
-    node.broadcast.write("new validator connected with ip " + newpeer.socket.host + " and port " + newpeer.socket.port)
+
 })
 
 node.on('disconnect', function() {
@@ -21,15 +19,11 @@ node.on('disconnect', function() {
 
 // Broadcast is a stream
 process.stdin.pipe(node.broadcast).on('data', function(chunk) {
-    console.log("A peer told you: " + chunk);
-    var peers = node.peers.list;
-    var ips = "";
-    for (peer in peers) {
-        ips += peer.socket;
+    console.log(chunk.toString('utf8'));
+    let message = JSON.parse(chunk.toString('utf8'));
+    if (message.type === "new") {
+        node.broadcast.write("new validator connected with ip " + message.ip + " and port " + message.port);
     }
-    console.log("Current ip's are: " + peers[0].socket.host + ", port: " +
-        peers[0].socket.port);
-
 });
 //Start the darn thing
 node.start()
