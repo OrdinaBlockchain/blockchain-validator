12595bf224428c0cf2f19ebce1cb9c136a6424f7
Michiel van Eijkeren <m.vaneijkeren@gmail.com>
2018-04-10 13:15:49 +0200

    Front-end for adding transactions

--- validator.js/validator.js
+++ validator.js/validator.js
@@ -1,11 +1,103 @@
 'use strict';
 
 require('dotenv').config();
-
+const url = require('url');
+const path = require('path');
+const electron = require('electron.js');
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 const Receiver = require('./services/receiver.js');
 const Sender = require('./services/sender');
 const NodeManager = require('./services/nodeManager.js');
 const prompt = require('prompt');
+
+let mainWindow;
+let addTransactionWindow;
+// listen for app to be ready
+app.on('ready', function() {
+    // new window
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true,
+    }));
+    // Quit app when closed
+    mainWindow.on('closed', function() {
+        app.quit();
+    });
+    // Build menu from template
+    const walletMenu = Menu.buildFromTemplate(walletMenuTemplate);
+    Menu.setApplicationMenu(walletMenu);
+});
+
+/**
+ * create add transaction window
+ */
+function createAddTransactionWindow() {
+    addTransactionWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add transaction',
+    });
+    addTransactionWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addTransactionWindow.html'),
+        protocol: 'file:',
+        slashes: true,
+    }));
+    // handle garbage collection
+    addTransactionWindow.on('closed', function() {
+        addTransactionWindow = null;
+    });
+}
+
+// catch transaction:add
+ipcMain.on('transaction:add', function(e, transaction) {
+    mainWindow.webContents.send('transaction:add', transaction);
+    addTransactionWindow.close();
+})
+
+// create menu template
+const walletMenuTemplate = [{
+    label: 'Options',
+    submenu: [
+        { label: 'Set public key' },
+        {
+            label: 'Add transaction',
+            click() {
+                createAddTransactionWindow();
+            }
+        },
+        { label: 'Clear public key' },
+        {
+            label: 'Quit',
+            click() {
+                app.quit();
+            }
+        },
+    ],
+}];
+// if mac, add empty object to menu
+if (process.platform == 'darwin') {
+    walletMenuTemplate.unshift({});
+}
+
+// add developer tools itme if not in prod
+if (process.env.NODE_ENV !== 'production') {
+    walletMenuTemplate.push({
+        label: 'Developer tools',
+        submenu: [{
+                label: 'Toggle Devtools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.webContents.openDevTools();
+                },
+            },
+            {
+                role: 'reload',
+            },
+        ],
+    });
+}
 
 prompt.start();
 
@@ -49,7 +141,8 @@ function promptLocations(isBackup) {
             configure();
         } else {
             prompt.get(['first_backup_host', 'first_backup_port',
-            'second_backup_host', 'second_backup_port'], (err, result) => {
+                'second_backup_host', 'second_backup_port',
+            ], (err, result) => {
                 if (err) {
                     return onErr(err);
                 }
@@ -87,4 +180,4 @@ function configure() {
 function onErr(err) {
     console.log(err);
     return 1;
-  }
+}
