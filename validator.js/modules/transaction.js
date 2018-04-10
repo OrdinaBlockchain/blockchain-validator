5e7493ca8908b9198e5479b5acb675b2f577aee1
Vidovitsch <david.de.bekker@gmail.com>
2018-04-10 12:07:16 +0200

    security.js partly refactoring

--- validator.js/modules/transaction.js
+++ validator.js/modules/transaction.js
@@ -2,41 +2,51 @@ let sec = require('./security');
 /**
  * Default message
  */
-var verified = false;
+let verified = false;
+
+/**
+ *
+ */
 class Transaction {
     /**
-     * 
-     * @param {*} senderpubkey 
-     * @param {*} data 
+     *
+     * @param {*} senderpubkey
+     * @param {*} data
      */
     constructor(senderpubkey, data) {
         this._senderpubkey = senderpubkey;
         this._data = data;
-        this._receiveraddress = data.receiver
+        this._receiveraddress = data.receiver;
         this._signature = data.signature;
         this._amount = data.amount;
         this._timestamp = data.timestamp;
     }
 
+    /**
+     * @return {*}
+     */
     getTimeStamp() {
         return this._timestamp;
-    };
+    }
 
+    /**
+     * @return {*}
+     */
     verify() {
         if (!this.verified) {
-            if (sec.Verify(this._signature, this._senderpubkey) !== "") {
-                console.log("Succesfully verified signature!");
+            if (sec.Verify(this._signature, this._senderpubkey) !== '') {
+                console.log('Succesfully verified signature!');
                 verified = true;
                 return true;
             } else {
-                console.log("Signature invalid!");
+                console.log('Signature invalid!');
                 return false;
             }
         } else {
-            console.log("Transaction already verified")
+            console.log('Transaction already verified');
             return true;
         }
-    };
+    }
 }
 
 module.exports = Transaction;
